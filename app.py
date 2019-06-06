f0769a45a1ffbe2f8911ced558648d5f8aaa199c
Gonçalo Malheiro <malheirogoncalo9@gmail.com>
2019-06-06 16:47:11 +0100

    6 jun

--- app.py
+++ app.py
@@ -2,36 +2,38 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 def gravar(v1, v2, v3):
     import sqlite3
     ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
-    db.execute("CREATE TABLE IF NOT EXISTS usr (nome text,email text,pass text)")
+    db.execute("CREATE TABLE IF NOT EXISTS usr (nome text,email text, passe text)")
     db.execute("INSERT INTO usr VALUES (?, ?, ?)", (v1, v2, v3))
     ficheiro.commit()
     ficheiro.close()
+
 
 def existe(v1):
     import sqlite3
     ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
-    db.execute("SELECT * FROM usr WHERE nome =?", (v1,))
+    db.execute("SELECT * FROM usr WHERE nome = ?", (v1,))
     valor = db.fetchone()
     ficheiro.close()
     return valor
 
-def alterar(v1):
+
+def log(v1, v2):
     import sqlite3
     ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
-    db.execute("SELECT * INTO usr SET pass = ? WHERE nome =?", (v1))
-    valor = 0
-    ficheiro.commit()
+    db.execute("SELECT * FROM usr WHERE nome = ? and passe = ?", (v1, v2,))
+    valor = db.fetchone()
     ficheiro.close()
     return valor
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/registo', methods=['GET', 'POST'])
 def route():
     erro = None
     if request.method == 'POST':
@@ -40,25 +42,54 @@ def route():
         v3 = request.form['passe']
         v4 = request.form['cpasse']
         if existe(v1):
-            erro = 'O Utilizador ja existe.'
+            erro = 'O Utilizador já existe.'
         elif v3 != v4:
-             erro = 'A palavra passe não coincide.'
+            erro = 'A palavra passe não coincide.'
         else:
-           gravar(v1, v2, v3)
+            gravar(v1, v2, v3)
     return render_template('registo.html', erro=erro)
 
-@app.route('/newpass', methods=['GET', 'POST'])
-def newpass():
+
+def alterar(v1, v2):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("UPDATE usr SET passe = ? WHERE nome = ?", (v2, v1))
+    ficheiro.commit()
+    ficheiro.close()
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['utilizador']
+        v2 = request.form['passe']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif not log(v1, v2):
+            erro = 'A palavra passe está errada.'
+        else:
+            erro = 'Bem-Vindo.'
+    return render_template('login.html', erro=erro)
+
+
+
+@app.route('/newpasse', methods=['GET', 'POST'])
+def newpasse():
     erro = None
     if request.method == 'POST':
         v1 = request.form['utilizador']
         v2 = request.form['passe']
         v3 = request.form['cpasse']
-        if v2 != v3:
-             erro = 'A palavra passe não coincide.'
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif v2 != v3:
+            erro = 'A palavra passe não coincide.'
         else:
-           alterar(v1, v2)
-    return render_template('newpass.html', erro=erro)
+            alterar(v1, v2)
+    return render_template('newpasse.html', erro=erro)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
